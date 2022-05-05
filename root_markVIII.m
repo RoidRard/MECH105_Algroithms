26f559b1f623bb8327145f7fb409097ee9647d04
RoidRard <104743635+RoidRard@users.noreply.github.com>
2022-05-04 21:39:52 -0600

    Delete root_markVIII.m

--- root_markVIII.m
+++
@@ -1,6 +0,0 @@
-function [xr] = root_markVIII(x1,x2,x3,y1,y2,y3)
-%ROOT_MARKVIII Summary of this function goes here
-%   Detailed explanation goes here
-    xr= (y2*y3/((y1-y2)*(y1-y3))*x1)+(y1*y3/((y2-y1)*(y2-y3))*x2)+(y1*y2/((y3-y1)*(y3-y2))*x3)
-end
-
